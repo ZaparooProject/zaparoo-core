191df0110e7276337c93e3932efa78b46eb6fa1b
Callan Barrett <callan@zoocar.org>
2025-02-18 22:43:40 +0800

    initial list picker test

--- pkg/configui/loader.go
+++ pkg/configui/loader.go
@@ -58,3 +58,53 @@ func LoaderUI(text string) {
 		panic(err)
 	}
 }
+
+func PickerUI() {
+	items := []string{
+		"Option 1",
+		"Option 2",
+		"Option 3",
+		"Option 4",
+		"Option 5",
+	}
+
+	app := tview.NewApplication()
+
+	setTheme()
+
+	list := tview.NewList()
+
+	for _, item := range items {
+		list.AddItem(item, "asdf", 0, func() {
+			app.Stop()
+		})
+	}
+
+	go func() {
+		time.Sleep(30 * time.Second)
+		app.Stop()
+	}()
+
+	list.SetDrawFunc(func(screen tcell.Screen, x, y, w, h int) (int, int, int, int) {
+		longest := 2
+		for _, item := range items {
+			if len(item) > longest {
+				longest = len(item)
+			}
+		}
+		x += (w - longest) / 2
+		y += (h - (list.GetItemCount() * 2)) / 2
+		return x, y, w, h
+	})
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEsc || event.Rune() == 'q' {
+			app.Stop()
+		}
+		return event
+	})
+
+	if err := app.SetRoot(list, true).Run(); err != nil {
+		panic(err)
+	}
+}
