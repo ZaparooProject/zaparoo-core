c6100ed02316b804235aa160c8f50471ccca4b53
Callan Barrett <callan@zoocar.org>
2025-03-02 19:25:10 +0800

    Adding zap link action api endpoint, refactor to avoid cyclic deps

--- pkg/api/methods/run.go
+++ pkg/api/methods/run.go
@@ -4,8 +4,15 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/ZaparooProject/zaparoo-core/pkg/api/client"
+	"github.com/ZaparooProject/zaparoo-core/pkg/database/systemdefs"
+	"github.com/ZaparooProject/zaparoo-core/pkg/platforms"
+	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -18,6 +25,8 @@ import (
 	"github.com/ZaparooProject/zaparoo-core/pkg/service/state"
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog/log"
+
+	widgetModels "github.com/ZaparooProject/zaparoo-core/pkg/configui/widgets/models"
 )
 
 var (
@@ -25,6 +34,11 @@ var (
 	ErrInvalidParams = errors.New("invalid params")
 	ErrNotAllowed    = errors.New("not allowed")
 )
+
+var MediaSafeList = []string{
+	"https://cdn.zaparoo.com",
+	"https://secure.cdn.zaparoo.com",
+}
 
 func HandleRun(env requests.RequestEnv) (any, error) {
 	log.Info().Msg("received run request")
@@ -134,4 +148,207 @@ func HandleRunRest(
 func HandleStop(env requests.RequestEnv) (any, error) {
 	log.Info().Msg("received stop request")
 	return nil, env.Platform.KillLauncher()
+}
+
+func HandleRunLinkAction(env requests.RequestEnv) (any, error) {
+	log.Info().Msg("received run link action request")
+
+	if len(env.Params) == 0 {
+		return nil, ErrMissingParams
+	}
+
+	var t tokens.Token
+
+	var params models.ZapLinkAction
+	err := json.Unmarshal(env.Params, &params)
+	if err != nil {
+		return nil, ErrInvalidParams
+	}
+
+	method := strings.ToLower(params.Method)
+	switch method {
+	case models.ZapLinkActionZapScript:
+		var zsp models.ZapScriptParams
+		err = json.Unmarshal(params.Params, &zsp)
+		if err != nil {
+			return nil, fmt.Errorf("error unmarshalling zapscript params: %w", err)
+		}
+		t.Text = zsp.ZapScript
+	case models.ZapLinkActionMedia:
+		// TODO: this will timeout on large downloads
+		t.Text, err = InstallRunMedia(env.Config, env.Platform, params)
+		if err != nil {
+			return nil, fmt.Errorf("error installing and running media: %w", err)
+		}
+	default:
+		return "", fmt.Errorf("unknown link action: %s", method)
+	}
+
+	t.ScanTime = time.Now()
+	t.FromAPI = true
+
+	env.State.SetActiveCard(t)
+	env.TokenQueue <- t
+
+	return nil, nil
+}
+
+func InstallRunMedia(
+	cfg *config.Instance,
+	pl platforms.Platform,
+	action models.ZapLinkAction,
+) (string, error) {
+	if pl.Id() != "mister" {
+		return "", errors.New("media install only supported for mister")
+	}
+
+	var mp models.MediaParams
+	err := json.Unmarshal(action.Params, &mp)
+	if err != nil {
+		return "", fmt.Errorf("error unmarshalling media params: %w", err)
+	}
+
+	isSafe := false
+	if mp.Url != nil {
+		log.Debug().Msgf("checking media download url: %s", *mp.Url)
+
+		for _, safe := range MediaSafeList {
+			if strings.HasPrefix(*mp.Url, safe) {
+				isSafe = true
+				break
+			}
+		}
+
+		if !isSafe {
+			return "", errors.New("media download not in safe list")
+		}
+	}
+
+	if mp.Url == nil {
+		return "", errors.New("media download url is empty")
+	}
+
+	system, err := systemdefs.GetSystem(mp.System)
+	if err != nil {
+		return "", fmt.Errorf("error getting system: %w", err)
+	}
+
+	var launchers []platforms.Launcher
+	for _, l := range pl.Launchers() {
+		if l.SystemId == system.Id {
+			launchers = append(launchers, l)
+		}
+	}
+
+	if len(launchers) == 0 {
+		return "", fmt.Errorf("no launchers for system: %s", system.Id)
+	}
+
+	// just use the first launcher for now
+	launcher := launchers[0]
+
+	if launcher.Folders == nil {
+		return "", errors.New("no folders for launcher")
+	}
+
+	// just use the first folder for now
+	folder := launcher.Folders[0]
+
+	name := filepath.Base(*mp.Url)
+
+	// roots := pl.RootDirs(cfg)
+
+	// if len(roots) == 0 {
+	// 	return "", errors.New("no root dirs")
+	// }
+
+	// root := roots[0]
+
+	root := "/media/fat/games" // TODO: this is hardcoded for now
+
+	path := filepath.Join(root, folder, name)
+
+	log.Debug().Msgf("media path: %s", path)
+
+	// check if the file already exists
+	if _, err := os.Stat(path); err == nil {
+		return path, nil
+	} else if !os.IsNotExist(err) {
+		return "", fmt.Errorf("error checking file: %w", err)
+	}
+
+	// download the file
+	log.Info().Msgf("downloading media: %s", *mp.Url)
+
+	// display loading dialog
+	argsPath := filepath.Join(pl.TempDir(), "loader.json")
+	completePath := filepath.Join(pl.TempDir(), ".loader-complete")
+	args := widgetModels.LoaderArgs{
+		Text:     fmt.Sprintf("Downloading %s...", mp.Name),
+		Complete: completePath,
+	}
+	argsJson, err := json.Marshal(args)
+	if err != nil {
+		return "", fmt.Errorf("error marshalling loader args: %w", err)
+	}
+	err = os.WriteFile(argsPath, argsJson, 0644)
+	if err != nil {
+		return "", fmt.Errorf("error writing loader args: %w", err)
+	}
+	// this is either the smartest or dumbest thing i ever came up with
+	text := fmt.Sprintf("**mister.script:zaparoo.sh -show-loader %s", argsPath)
+	apiArgs := models.RunParams{
+		Text: &text,
+	}
+	ps, err := json.Marshal(apiArgs)
+	if err != nil {
+		log.Error().Err(err).Msg("error creating run params")
+	}
+	_, err = client.LocalClient(cfg, "run", string(ps))
+	if err != nil {
+		log.Error().Err(err).Msg("error running local client")
+	}
+
+	resp, err := http.Get(*mp.Url)
+	if err != nil {
+		return "", fmt.Errorf("error getting url: %w", err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Error().Err(err).Msgf("closing body")
+		}
+	}(resp.Body)
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("invalid status code: %d", resp.StatusCode)
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return "", fmt.Errorf("error creating file: %w", err)
+	}
+
+	defer func(File *os.File) {
+		err := File.Close()
+		if err != nil {
+			log.Error().Err(err).Msgf("closing file")
+		}
+	}(file)
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error copying file: %w", err)
+	}
+
+	// remove loading dialog
+	err = os.Remove(argsPath)
+	if err != nil {
+		return "", fmt.Errorf("error removing loader args: %w", err)
+	}
+	err = os.WriteFile(completePath, []byte{}, 0644)
+	if err != nil {
+		return "", fmt.Errorf("error writing loader complete: %w", err)
+	}
+
+	return path, nil
 }
