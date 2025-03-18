0a291ab39ab4b864b7a072bfbaf325c2e274d159
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-03-18 16:53:00 +0100

    cant even save because the stupid linter

--- pkg/simplegui/simplegui.go
+++ pkg/simplegui/simplegui.go
@@ -1,3 +1,5 @@
+package simplegui
+
 /*
 Zaparoo Core
 Copyright (C) 2023, 2024 Callan Barrett
@@ -17,8 +19,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Zaparoo Core.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-package simplegui
 
 import (
 	"os/exec"
