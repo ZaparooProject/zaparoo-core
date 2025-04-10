274c6f6bd1949330cba26deedaac0c594d20515c
Callan Barrett <callan@zoocar.org>
2025-04-10 15:22:01 +0800

    Add platform chimeraos

---
+++ cmd/chimeraos/main.go
@@ -0,0 +1,105 @@
+/*
+Zaparoo Core
+Copyright (C) 2023 Gareth Jones
+Copyright (C) 2023-2025 Callan Barrett
+
+This file is part of Zaparoo Core.
+
+Zaparoo Core is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Zaparoo Core is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Zaparoo Core.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/ZaparooProject/zaparoo-core/pkg/cli"
+	"github.com/ZaparooProject/zaparoo-core/pkg/config"
+	"github.com/ZaparooProject/zaparoo-core/pkg/platforms/chimeraos"
+	"github.com/ZaparooProject/zaparoo-core/pkg/platforms/linux/installer"
+	"github.com/ZaparooProject/zaparoo-core/pkg/service"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	defer close(sigs)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	pl := &chimeraos.Platform{}
+	flags := cli.SetupFlags()
+
+	doInstall := flag.Bool("install", false, "configure system for zaparoo")
+	doUninstall := flag.Bool("uninstall", false, "revert zaparoo system configuration")
+	asDaemon := flag.Bool("daemon", false, "run zaparoo in daemon mode")
+
+	flags.Pre(pl)
+
+	if *doInstall {
+		err := installer.CLIInstall()
+		if err != nil {
+			os.Exit(1)
+		} else {
+			os.Exit(0)
+		}
+	} else if *doUninstall {
+		err := installer.CLIUninstall()
+		if err != nil {
+			os.Exit(1)
+		} else {
+			os.Exit(0)
+		}
+	}
+
+	if os.Geteuid() == 0 {
+		_, _ = fmt.Fprintf(os.Stderr, "Zaparoo must not be run as root\n")
+		os.Exit(1)
+	}
+
+	// only difference with daemon mode right now is no log pretty printing
+	// TODO: launch simple gui
+	// TODO: fork service if it's not running
+	logWriters := []io.Writer{zerolog.ConsoleWriter{Out: os.Stderr}}
+	if *asDaemon {
+		logWriters = []io.Writer{os.Stderr}
+	}
+
+	cfg := cli.Setup(
+		pl,
+		config.BaseDefaults,
+		logWriters,
+	)
+
+	flags.Post(cfg, pl)
+
+	stop, err := service.Start(pl, cfg)
+	if err != nil {
+		log.Error().Err(err).Msg("error starting service")
+		os.Exit(1)
+	}
+
+	<-sigs
+	err = stop()
+	if err != nil {
+		log.Error().Err(err).Msg("error stopping service")
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
---
+++ pkg/platforms/chimeraos/platform.go
@@ -0,0 +1,240 @@
+/*
+Zaparoo Core
+Copyright (C) 2024, 2025 Callan Barrett
+
+This file is part of Zaparoo Core.
+
+Zaparoo Core is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Zaparoo Core is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Zaparoo Core.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package chimeraos
+
+import (
+	"errors"
+	"fmt"
+	widgetModels "github.com/ZaparooProject/zaparoo-core/pkg/configui/widgets/models"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/ZaparooProject/zaparoo-core/pkg/readers/libnfc"
+	"github.com/ZaparooProject/zaparoo-core/pkg/readers/optical_drive"
+	"github.com/ZaparooProject/zaparoo-core/pkg/service/tokens"
+	"github.com/ZaparooProject/zaparoo-core/pkg/utils"
+	"github.com/adrg/xdg"
+	"github.com/rs/zerolog/log"
+
+	"github.com/ZaparooProject/zaparoo-core/pkg/api/models"
+
+	"github.com/ZaparooProject/zaparoo-core/pkg/config"
+	"github.com/ZaparooProject/zaparoo-core/pkg/platforms"
+	"github.com/ZaparooProject/zaparoo-core/pkg/readers"
+	"github.com/ZaparooProject/zaparoo-core/pkg/readers/file"
+	"github.com/ZaparooProject/zaparoo-core/pkg/readers/simple_serial"
+)
+
+type Platform struct {
+	tempDir string
+}
+
+func (p *Platform) Id() string {
+	return platforms.PlatformIDChimeraOS
+}
+
+func (p *Platform) SupportedReaders(cfg *config.Instance) []readers.Reader {
+	return []readers.Reader{
+		file.NewReader(cfg),
+		simple_serial.NewReader(cfg),
+		libnfc.NewReader(cfg),
+		optical_drive.NewReader(cfg),
+	}
+}
+
+func (p *Platform) StartPre(_ *config.Instance) error {
+	tempDir, err := os.MkdirTemp("", "zaparoo-")
+	if err != nil {
+		return fmt.Errorf("failed to create temp dir: %w", err)
+	}
+	p.tempDir = tempDir
+
+	for _, dir := range []string{p.DataDir(), p.LogDir(), p.ConfigDir()} {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			return fmt.Errorf("failed to create dir: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (p *Platform) StartPost(_ *config.Instance, _ chan<- models.Notification) error {
+	return nil
+}
+
+func (p *Platform) Stop() error {
+	err := os.RemoveAll(p.TempDir())
+	if err != nil {
+		return err
+	}
+	p.tempDir = ""
+
+	return nil
+}
+
+func (p *Platform) AfterScanHook(_ tokens.Token) error {
+	return nil
+}
+
+func (p *Platform) ReadersUpdateHook(_ map[string]*readers.Reader) error {
+	return nil
+}
+
+func (p *Platform) RootDirs(_ *config.Instance) []string {
+	return []string{}
+}
+
+func (p *Platform) ZipsAsDirs() bool {
+	return false
+}
+
+func (p *Platform) DataDir() string {
+	return filepath.Join(xdg.DataHome, config.AppName)
+}
+
+func (p *Platform) LogDir() string {
+	return filepath.Join(xdg.DataHome, config.AppName)
+}
+
+func (p *Platform) ConfigDir() string {
+	return filepath.Join(xdg.ConfigHome, config.AppName)
+}
+
+func (p *Platform) TempDir() string {
+	if p.tempDir == "" {
+		log.Warn().Msg("temp dir not set")
+	}
+	return p.tempDir
+}
+
+func (p *Platform) NormalizePath(_ *config.Instance, path string) string {
+	return path
+}
+
+func LaunchMenu() error {
+	return nil
+}
+
+func (p *Platform) KillLauncher() error {
+	return nil
+}
+
+func (p *Platform) GetActiveLauncher() string {
+	return ""
+}
+
+func (p *Platform) PlayFailSound(_ *config.Instance) {
+}
+
+func (p *Platform) PlaySuccessSound(_ *config.Instance) {
+}
+
+func (p *Platform) ActiveSystem() string {
+	return ""
+}
+
+func (p *Platform) ActiveGame() string {
+	return ""
+}
+
+func (p *Platform) ActiveGameName() string {
+	return ""
+}
+
+func (p *Platform) ActiveGamePath() string {
+	return ""
+}
+
+func (p *Platform) LaunchSystem(_ *config.Instance, _ string) error {
+	return nil
+}
+
+func (p *Platform) LaunchFile(cfg *config.Instance, path string) error {
+	launchers := utils.PathToLaunchers(cfg, p, path)
+	if len(launchers) == 0 {
+		return errors.New("no launcher found")
+	}
+	launcher := launchers[0]
+
+	if launcher.AllowListOnly && !cfg.IsLauncherFileAllowed(path) {
+		return errors.New("file not allowed: " + path)
+	}
+
+	log.Info().Msgf("launching file with %s: %s", launcher.Id, path)
+	return launcher.Launch(cfg, path)
+}
+
+func (p *Platform) KeyboardInput(_ string) error {
+	return nil
+}
+
+func (p *Platform) KeyboardPress(name string) error {
+	return nil
+}
+
+func (p *Platform) GamepadPress(name string) error {
+	return nil
+}
+
+func (p *Platform) ForwardCmd(_ platforms.CmdEnv) error {
+	return nil
+}
+
+func (p *Platform) LookupMapping(_ tokens.Token) (string, bool) {
+	return "", false
+}
+
+func (p *Platform) Launchers() []platforms.Launcher {
+	return []platforms.Launcher{
+		{
+			Id:            "Generic",
+			Extensions:    []string{".sh"},
+			AllowListOnly: true,
+			Launch: func(cfg *config.Instance, path string) error {
+				return exec.Command(path).Start()
+			},
+		},
+	}
+}
+
+func (p *Platform) ShowNotice(
+	_ *config.Instance,
+	_ widgetModels.NoticeArgs,
+) (func() error, time.Duration, error) {
+	return nil, 0, nil
+}
+
+func (p *Platform) ShowLoader(
+	_ *config.Instance,
+	_ widgetModels.NoticeArgs,
+) (func() error, error) {
+	return nil, nil
+}
+
+func (p *Platform) ShowPicker(
+	_ *config.Instance,
+	_ widgetModels.PickerArgs,
+) error {
+	return nil
+}
