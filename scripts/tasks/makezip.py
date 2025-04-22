51b5042ba4198fd8774e903f8395975f3e47c7f7
Callan Barrett <callan@zoocar.org>
2025-04-22 15:10:00 +0800

    Cross-platform script for building zips

--- scripts/tasks/makezip.py
+++ scripts/tasks/makezip.py
@@ -1,6 +1,8 @@
 import os
+import shutil
 import sys
 import urllib.request
+import zipfile
 
 base_url: str = "https://github.com/ZaparooProject/zaparoo.org/raw/refs/heads/main/docs/platforms/"
 platform_docs: dict[str, str] = {
@@ -27,7 +29,7 @@ def strip_frontmatter(content: str) -> str:
     return content
 
 
-def download_doc(platform_id: str):
+def download_doc(platform_id: str, to_dir: str):
     if platform_id not in platform_docs:
         raise ValueError(f"Platform '{platform_id}' not found in the platforms list.")
 
@@ -40,24 +42,41 @@ def download_doc(platform_id: str):
     if file_name.lower().endswith(".mdx"):
         content = strip_frontmatter(content)
 
-    with open("README.txt", "w", encoding="utf-8") as file:
+    with open(os.path.join(to_dir, "README.txt"), "w", encoding="utf-8") as file:
         file.write(content.strip() + "\n")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        raise ValueError("Provide platform as argument.")
+    if len(sys.argv) < 5:
+        raise ValueError("Usage: makezip.py <platform> <build_dir> <app_bin> <zip_name>")
 
     platform = sys.argv[1]
-    target_dir = sys.argv[2] if len(sys.argv) > 2 else "."
+    build_dir = sys.argv[2]
+    app_bin = sys.argv[3]
+    zip_name = sys.argv[4]
 
-    if not os.path.isdir(target_dir):
-        raise NotADirectoryError(f"The specified directory '{target_dir}' does not exist.")
+    if not os.path.isdir(build_dir):
+        raise NotADirectoryError(f"The specified directory '{build_dir}' does not exist.")
 
-    os.chdir(target_dir)
+    license_path = os.path.join(build_dir, "LICENSE.txt")
+    if not os.path.isfile(license_path):
+        shutil.copy("LICENSE", license_path)
 
-    readme_path = os.path.join(target_dir, "README.txt")
+    app_path = os.path.join(build_dir, app_bin)
+    if not os.path.isfile(app_path):
+        raise FileNotFoundError(f"The specified binary file '{app_path}' does not exist.")
+
+    zip_path = os.path.join(build_dir, zip_name)
+    if os.path.isfile(zip_path):
+        os.remove(zip_path)
+
+    readme_path = os.path.join(build_dir, "README.txt")
     if os.path.exists(readme_path):
         print(f"File '{readme_path}' already exists. Skipping download.")
     else:
-        download_doc(platform)
+        download_doc(platform, build_dir)
+
+    with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as dist:
+        dist.write(app_path, arcname=os.path.basename(app_path))
+        dist.write(license_path, arcname=os.path.basename(license_path))
+        dist.write(readme_path, arcname=os.path.basename(readme_path))
