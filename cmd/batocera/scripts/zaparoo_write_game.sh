555bb2af562a7e6c1bc5a06dbc17a971ceac094a
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-04-25 13:28:45 +0200

    make it work no api

--- cmd/batocera/scripts/zaparoo_write_game.sh
+++ cmd/batocera/scripts/zaparoo_write_game.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Retrieve latest selected item
-item="$(curl -sSL http://127.0.0.1:7497/selected-item)"
+item="$(cat /var/run/zaparoo.tmp)"
 
 # POST to batocera to put a card on the reader
 curl -X POST --data "Place a tag on the NFC writer to write '${item}'. Write will timeout in 30 seconds" http://127.0.0.1:1234/messagebox
