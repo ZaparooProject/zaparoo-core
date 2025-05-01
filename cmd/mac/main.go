00541efb0bb393d817c1ec3cf763678935a0132c
Callan Barrett <callan@zoocar.org>
2025-05-01 11:17:49 +0800

    About dialog, build script fixes

--- cmd/mac/main.go
+++ cmd/mac/main.go
@@ -27,18 +27,21 @@ import (
 	"github.com/ZaparooProject/zaparoo-core/pkg/api/client"
 	"github.com/ZaparooProject/zaparoo-core/pkg/api/models"
 	"github.com/ZaparooProject/zaparoo-core/pkg/cli"
+	"github.com/ZaparooProject/zaparoo-core/pkg/config"
 	"github.com/ZaparooProject/zaparoo-core/pkg/platforms"
 	"github.com/ZaparooProject/zaparoo-core/pkg/platforms/mac"
 	"github.com/ZaparooProject/zaparoo-core/pkg/simplegui"
+	"github.com/ZaparooProject/zaparoo-core/pkg/utils"
+	"github.com/nixinwang/dialog"
 	"github.com/rs/zerolog/log"
+	"golang.design/x/clipboard"
 	"io"
 	"os"
 	"os/exec"
 	"os/signal"
 	"path/filepath"
 	"syscall"
-
-	"github.com/ZaparooProject/zaparoo-core/pkg/config"
+	"time"
 
 	"github.com/ZaparooProject/zaparoo-core/pkg/service"
 
@@ -64,17 +67,47 @@ func systrayOnReady(cfg *config.Instance, pl platforms.Platform) func() {
 		systray.SetIcon(systrayIcon)
 
 		mWebUI := systray.AddMenuItem("Web UI", "Open Zaparoo web UI")
-		mReloadConfig := systray.AddMenuItem("Reload Config", "Reload Core config file and mappings")
+		address := "Unknown"
+		ip, err := utils.GetLocalIp()
+		if err == nil {
+			address = ip.String()
+		}
+		mAddress := systray.AddMenuItem("Address: "+address, "")
 		systray.AddSeparator()
+
 		mEditConfig := systray.AddMenuItem("Edit Config", "Edit Core config file")
+		mReloadConfig := systray.AddMenuItem("Reload Config", "Reload Core config file and mappings")
 		mOpenMappings := systray.AddMenuItem("Open Mappings", "Open Core mappings directory")
 		mOpenLog := systray.AddMenuItem("Open Log", "Open Core log file")
+
+		if cfg.DebugLogging() {
+			systray.AddSeparator()
+		}
+		mOpenDataDir := systray.AddMenuItem("Open Data (Debug)", "Open Core data directory")
+		mOpenDataDir.Hide()
+		if cfg.DebugLogging() {
+			mOpenDataDir.Show()
+		}
+
+		systray.AddSeparator()
+		mVersion := systray.AddMenuItem("Version "+config.AppVersion, "")
+		mVersion.Disable()
+		mAbout := systray.AddMenuItem("About Zaparoo Core", "")
+
 		systray.AddSeparator()
 		mQuit := systray.AddMenuItem("Quit", "Quit and stop Zaparoo service")
 
 		go func() {
 			for {
 				select {
+				case <-mAddress.ClickedCh:
+					err := clipboard.Init()
+					if err != nil {
+						log.Error().Err(err).Msg("failed to initialize clipboard")
+						continue
+					}
+					clipboard.Write(clipboard.FmtText, []byte(address))
+					// TODO: send notification
 				case <-mWebUI.ClickedCh:
 					url := fmt.Sprintf("http://localhost:%d/app/", cfg.ApiPort())
 					err := exec.Command("open", url).Start()
@@ -103,6 +136,18 @@ func systrayOnReady(cfg *config.Instance, pl platforms.Platform) func() {
 					} else {
 						log.Info().Msg("reloaded config")
 					}
+				case <-mOpenDataDir.ClickedCh:
+					err := exec.Command("open", pl.Settings().DataDir).Start()
+					if err != nil {
+						log.Error().Err(err).Msg("failed to open data dir")
+					}
+				case <-mAbout.ClickedCh:
+					msg := "Zaparoo Core\n" +
+						"Version v%s\n\n" +
+						"© %d Zaparoo Contributors\n" +
+						"License: GPLv3\n\n" +
+						"www.zaparoo.org"
+					dialog.Message(msg, config.AppVersion, time.Now().Year()).Title("About Zaparoo Core").Info()
 				case <-mQuit.ClickedCh:
 					systray.Quit()
 				}
@@ -128,13 +173,13 @@ func main() {
 	appMode := flag.Bool(
 		"app",
 		false,
-		"run Zaparoo service with system tray",
+		"run Zaparoo service as daemon in menu bar",
 	)
 
 	flags.Pre(pl)
 
 	var logWriters []io.Writer
-	if *daemonMode {
+	if *daemonMode || *appMode {
 		logWriters = []io.Writer{os.Stderr}
 	}
 
