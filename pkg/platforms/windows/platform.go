bf14044207bbbfce13ce054165d7ff67af008bdc
Callan Barrett <callan@zoocar.org>
2025-11-14 08:06:04 +0800

    refactor(platforms/windows): simplify command execution for browser launch

--- pkg/platforms/windows/platform.go
+++ pkg/platforms/windows/platform.go
@@ -585,12 +585,7 @@ func (p *Platform) Launchers(cfg *config.Instance) []platforms.Launcher {
 			Schemes:   []string{"http", "https"},
 			Lifecycle: platforms.LifecycleFireAndForget,
 			Launch: func(_ *config.Instance, path string) (*os.Process, error) {
-				//nolint:gosec // Safe: opens user-scanned URLs in default web browser
-				cmd := exec.CommandContext(context.Background(),
-					"cmd", "/c",
-					"start",
-					path,
-				)
+				cmd := exec.CommandContext(context.Background(), "cmd", "/c", "start", path)
 				cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 				err := cmd.Start()
 				if err != nil {
