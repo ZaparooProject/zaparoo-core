714b3c5ced25c7962ae2a7e5a6c12d489dab7174
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-12-03 01:00:19 +0100

    works

--- pkg/api/models/models.go
+++ pkg/api/models/models.go
@@ -79,7 +79,7 @@ const (
 	MethodReadersWrite         = "readers.write"
 	MethodReadersWriteCancel   = "readers.write.cancel"
 	MethodVersion              = "version"
-	MethodHealthCheck          = "healthCheck"
+	MethodHealthCheck          = "healthcheck"
 )
 
 type Notification struct {
