7b49e1e030ce8f3946f5ad1428505939adc3d82f
Callan Barrett <callan@zoocar.org>
2026-06-03 18:10:26 +0800

    feat(api): prioritize websocket requests

---
+++ pkg/api/request_priority.go
@@ -0,0 +1,95 @@
+// Zaparoo Core
+// Copyright (c) 2026 The Zaparoo Project Contributors.
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This file is part of Zaparoo Core.
+//
+// Zaparoo Core is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Zaparoo Core is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Zaparoo Core.  If not, see <http://www.gnu.org/licenses/>.
+
+package api
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/ZaparooProject/zaparoo-core/v2/pkg/api/models"
+)
+
+type apiRequestPriority int
+
+const (
+	apiPriorityHigh apiRequestPriority = iota
+	apiPriorityNormal
+	apiPriorityLow
+)
+
+func classifyAPIMethod(method string) apiRequestPriority {
+	method = strings.ToLower(method)
+
+	switch method {
+	case models.MethodMediaTagsUpdate,
+		models.MethodRun,
+		models.MethodRunScript,
+		models.MethodLaunch,
+		models.MethodStop,
+		models.MethodConfirm,
+		models.MethodSettingsUpdate,
+		models.MethodPlaytimeLimitsUpdate,
+		models.MethodClientsDelete,
+		models.MethodInboxDelete,
+		models.MethodInboxClear,
+		models.MethodReadersWrite,
+		models.MethodReadersWriteCancel,
+		models.MethodMediaActiveUpdate,
+		models.MethodMediaControl,
+		models.MethodMappingsNew,
+		models.MethodMappingsDelete,
+		models.MethodMappingsUpdate,
+		models.MethodMappingsReload:
+		return apiPriorityHigh
+	case models.MethodMediaImage,
+		models.MethodMediaGenerate,
+		models.MethodMediaGenerateCancel,
+		models.MethodMediaGenerateResume,
+		models.MethodMediaIndex,
+		models.MethodMediaScrape,
+		models.MethodMediaScrapeStatus,
+		models.MethodMediaScrapeCancel,
+		models.MethodMediaScrapeResume,
+		models.MethodMediaCleanOrphans,
+		models.MethodSettingsLogsDownload:
+		return apiPriorityLow
+	default:
+		if strings.HasPrefix(method, "media.scrape") || strings.HasPrefix(method, "media.generate") {
+			return apiPriorityLow
+		}
+		return apiPriorityNormal
+	}
+}
+
+func methodFromAPIRequestPayload(msg []byte) string {
+	var req models.RequestObject
+	if err := json.Unmarshal(msg, &req); err != nil {
+		return ""
+	}
+	return strings.ToLower(req.Method)
+}
+
+func isImageAPIMethod(method string) bool {
+	return strings.EqualFold(method, models.MethodMediaImage)
+}
+
+func isMediaDBTransactionAPIMethod(method string) bool {
+	return strings.EqualFold(method, models.MethodMediaTagsUpdate)
+}
